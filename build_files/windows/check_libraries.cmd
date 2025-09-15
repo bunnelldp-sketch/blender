41502fd36d76ebdc107944e0c4bd063e8d713e5d
bunnelldp-sketch <bunnelldp@s.dcsdk12.org>
2025-09-15 14:43:35 -0600

    tffyiufctu

--- build_files/windows/check_libraries.cmd
+++ build_files/windows/check_libraries.cmd
@@ -15,21 +15,21 @@ if NOT EXIST "%BUILD_VS_LIBDIR%\.git" (
 		echo.
 		echo "Would you like to download them? (y/n)"
 		echo "Y"
-		set /p Hello="Y"
-		if /I "!Hello!"=="Y" (
-			echo.
-			echo Downloading %BUILD_VS_LIBDIR% libraries, please wait.
-			echo.
-			echo *********************************************************
-			echo *                                                       *
-			echo * Note: Once the initial download finishes and you see  *
-			echo *       "Resolving deltas: 100%% (nnn/nnn) done"         *
-			echo *       a second, much larger, update will occur with   *
-			echo *       no visible updates. Please do not interrupt     *
-			echo *       this process. It may take over an hour to       *
-			echo *       complete depending on your internet connection. *
-			echo *                                                       *
-			echo *********************************************************
+
+
+		echo.
+		echo Downloading %BUILD_VS_LIBDIR% libraries, please wait.
+		echo.
+		echo *********************************************************
+		echo *                                                       *
+		echo * Note: Once the initial download finishes and you see  *
+		echo *       "Resolving deltas: 100%% (nnn/nnn) done"         *
+		echo *       a second, much larger, update will occur with   *
+		echo *       no visible updates. Please do not interrupt     *
+		echo *       this process. It may take over an hour to       *
+		echo *       complete depending on your internet connection. *
+		echo *                                                       *
+		echo *********************************************************
 :RETRY
 			"%GIT%" -C "%BLENDER_DIR%\" config --local "submodule.%BUILD_VS_LIBDIR%.update" "checkout"
 			set GIT_LFS_SKIP_SMUDGE=1
